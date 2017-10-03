fe7a077d578c3694ccb5f27573a41b9ca6bc53f3
Jack Hookham (PGT) <jackhookham@gmail.com>
2017-10-03 14:01:51 +0100

    Calculator

--- Lessons/Lesson 2 - Functions/Lesson 2 - Functions/functions.cpp
+++ Lessons/Lesson 2 - Functions/Lesson 2 - Functions/functions.cpp
@@ -4,14 +4,84 @@ using namespace std;
 
 inline int add(int x, int y) { return x + y; }
 inline int subtract(int x, int y);
-inline void product(int x, int y) { cout << x << " * " << y << " = " << x * y << "\n"; }
+//inline void product(int x, int y) { cout << x << " * " << y << " = " << x * y << "\n"; }
+inline int product(int x, int y) { return x * y; }
 inline double quotient(double x, double y) { return x / y; }
+
+void calculator()
+{
+	int x, y;
+	double z;
+	char op;
+
+	cout << "Enter an integer, x: ";
+	cin >> x;
+	while (cin.fail())
+	{
+		cout << "Not an integer\n";
+		cout << "Enter an integer, x: ";
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> x;
+	} 
+
+	cout << "Enter an integer, y: ";
+	cin >> y;
+	while (cin.fail())
+	{
+		cout << "Not an integer\n";
+		cout << "Enter an integer, y: ";
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> y;
+	}
+
+	bool validOp = false;
+
+	while (!validOp)
+	{
+		cout << "Enter an operator +, -, * or / : ";		
+		cin >> op;
+
+		switch (op)
+		{
+		case 43: //+
+			z = add(x, y);
+			cout << x << " + " << y << " = " << z << "\n";
+			validOp = true;
+			break;
+		case 45: //-
+			z = subtract(x, y);
+			cout << x << " - " << y << " = " << z << "\n";
+			validOp = true;
+			break;
+		case 42: //*
+			z = product(x, y);
+			cout << x << " * " << y << " = " << z << "\n";
+			validOp = true;
+			break;
+		case 47: // /
+			z = quotient((double)x, (double)y);
+			cout << x << " / " << y << " = " << z << "\n";
+			validOp = true;
+			break;
+		default:
+			cout << "Not a valid operator!\n";
+			break;
+		}
+	}
+
+	cin.ignore();
+	cin.get();
+}
 
 int main()
 {
+	calculator();
+
 	int a, b, c;
 
-	cout << "Please type in a number: ";
+	cout << "\nPlease type in a number: ";
 	cin >> a;
 	cout << "\nPlease type in another number: ";
 	cin >> b;
