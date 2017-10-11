310aa8a3e80e2086bcb28e5a1718729462938843
Jack Hookham (PGT) <jackhookham@gmail.com>
2017-10-11 12:26:51 +0100

    Inheritance q 4

--- Lessons/Lesson 10 - Inheritance/Lesson 10 - Inheritance/inheritance.cpp
+++ Lessons/Lesson 10 - Inheritance/Lesson 10 - Inheritance/inheritance.cpp
@@ -84,21 +84,99 @@ void ArmedEnemy::shoot() {
 	}
 }
 
+class Boss : public ArmedEnemy
+{
+public :
+	Boss(int hps, int ammo, int armour);
+	virtual ~Boss();
+	
+	virtual void setArmour(int armour);
+	virtual int getArmour();
+protected:
+	int armourLevel;
+};
+
+Boss::Boss(int hps, int ammo, int armour) :
+	ArmedEnemy(hps, ammo),
+	armourLevel(armour)
+{
+}
+
+Boss::~Boss()
+{
+}
+
+void Boss::setArmour(int armour)
+{
+	armourLevel = armour;
+}
+
+int Boss::getArmour()
+{
+	return armourLevel;
+}
+
 void some_function(Enemy& enemy) {
 	enemy.set_score(6);
 }
 
+void decrementHP(Enemy& enemy)
+{
+	enemy.set_hit_points(enemy.get_hit_points() - 1);
+}
+
 int main(void) {
 
-	ArmedEnemy* ae = new ArmedEnemy(2, 5);
-	ae->set_hit_points(3);
-	cout << "hit points = " << ae->get_hit_points() << "\n";
+	//ArmedEnemy* ae = new ArmedEnemy(2, 5);
+	//ae->set_hit_points(3);
+	//cout << "hit points = " << ae->get_hit_points() << "\n";
 
-	ae->shoot();
+	//ae->shoot();
 
-	some_function(*ae);
+	//some_function(*ae);
 
-	delete ae;
-	ae = NULL;
+	const int numArmedEnemies = 10;
+	ArmedEnemy* armedEnemies[numArmedEnemies];
+	for (int i = 0; i < numArmedEnemies; i++)
+	{
+		armedEnemies[i] = new ArmedEnemy(2, 5);
+	}
+
+	Boss* boss = new Boss(3, 6, 3);
+
+	const int numAllEnemies = 11;
+	Enemy* allEnemies[numAllEnemies];
+	allEnemies[0] = boss;
+	for (int i = 0; i < numArmedEnemies; i++)
+	{
+		allEnemies[i + 1] = armedEnemies[i];
+	}
+
+	for (int i = 0; i < numAllEnemies; i++)
+	{
+		cout << "HP " << i << ':' << allEnemies[i]->get_hit_points() << "\n";
+	}
+	cout << "\n";
+
+	for (int i = 0; i < numAllEnemies; i++)
+	{
+		decrementHP(*allEnemies[i]);
+	}
+
+	for (int i = 0; i < numAllEnemies; i++)
+	{
+		cout << "HP " << i << ':' << allEnemies[i]->get_hit_points() << "\n";
+	}
+
+	//delete ae;
+	//ae = NULL;
+	for (int i = 0; i < numAllEnemies; i++)
+	{
+		delete allEnemies[i];
+		allEnemies[i] = NULL;
+	}
+	cin.ignore();
+	cin.get();
+
 	return 0;
 }
